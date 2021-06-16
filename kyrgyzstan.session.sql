464f2dcd1137d5d5d42623a08b61d0690696de8b
ktemirbekovna <84611242+ktemirbekovna@users.noreply.github.com>
2021-06-16 13:27:45 +0600

    База данных Kyrgyzstan

---
+++ kyrgyzstan.session.sql
@@ -0,0 +1,19 @@
+-- CREATE TABLE developers(id SERIAL PRIMARY KEY,
+-- names VARCHAR,population INT);
+
+-- INSERT INTO developers(names, population)
+-- VALUES('Chuy',100000);
+-- INSERT INTO developers(names, population)
+-- VALUES('Osh',200000);
+-- INSERT INTO developers(names, population)
+-- VALUES('Naryn',300000);
+
+-- ALTER TABLE developers ADD COLUMN teams VARCHAR;
+
+-- ALTER TABLE developers RENAME COLUMN population TO participants;
+
+-- DELETE FROM developers WHERE participants = 300000;
+
+-- UPDATE developers SET participants = 7000 WHERE participants > 80000;
+
+-- SELECT * FROM developers;
